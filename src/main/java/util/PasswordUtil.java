0bc34aa25d6c3b51d4c9ea989c2efad0d2adedb6
kelt-ac <kachguer@gmail.com>
2026-02-13 07:20:00 +0100

    https://github.com/kelt-ac/Live-Chat.git

--- src/main/java/util/PasswordUtil.java
+++ src/main/java/util/PasswordUtil.java
@@ -2,4 +2,7 @@ package util;
 
 public class PasswordUtil {
 
+    public static String hashPassword(String password) {
+        return password;
+    }
 }
--- src/main/java/websocket/ChatEndpoint.java
+++ src/main/java/websocket/ChatEndpoint.java
@@ -1,4 +1,6 @@
 package websocket;
 
 public class ChatEndpoint {
+    public static void sendToUser(int i, String notification) {
+    }
 }
